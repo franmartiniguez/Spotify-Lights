4e00e8ff93f98db6a00b0887ecbe7d116c755b67
unknown <franmartiniguez@gmail.com>
2022-12-25 11:02:05 -0800

    quick fix

--- AlbumLights/main.py
+++ AlbumLights/main.py
@@ -76,19 +76,22 @@ def main():
     GPIO.setwarnings(False)
 
     pixels = neopixel.NeoPixel(board.D18, 60)
-    while True:
-        refresh_token = Refresh()
-        SPOTIFY_ACCESS_TOKEN = refresh_token.refresh()
-        current_track_art = get_current_track(SPOTIFY_ACCESS_TOKEN)
+    try:
+        while True:
+            refresh_token = Refresh()
+            SPOTIFY_ACCESS_TOKEN = refresh_token.refresh()
+            current_track_art = get_current_track(SPOTIFY_ACCESS_TOKEN)
 
-        if current_track_art == 'Unrecoverable Art':
-            color_values = [255, 255, 255]
-        elif current_track_art == 'No Track Playing':
-            color_values = [0, 0, 0]
-        else:
-            color_values = get_color(current_track_art)
-            
-        pixels.fill((color_values[0], color_values[1], color_values[2]))
-      
+            if current_track_art == 'Unrecoverable Art':
+                color_values = [255, 255, 255]
+            elif current_track_art == 'No Track Playing':
+                color_values = [0, 0, 0]
+            else:
+                color_values = get_color(current_track_art)
+                
+            pixels.fill((color_values[0], color_values[1], color_values[2]))
+    except:
+        GPIO.cleanup()
+        
 if __name__ == "__main__":
     main()
